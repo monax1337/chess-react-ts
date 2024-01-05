aa06bc2ddb06844a1d7401ef3e6a4b6d8c0f544a
Kruglikovskiy Maxim <maxfff03@gmail.com>
2024-01-05 18:01:40 +0300

    added board, figures and opportunity to highlight direction to move

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,24 @@
+import React, {FC} from 'react';
+import {Cell} from "../models/Cell";
+
+interface CellProps {
+    cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void;
+}
+
+
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
+    return (
+        <div
+            className={["cell", cell.color, selected ? "selected" : ""].join(' ')}
+            onClick={() => click(cell)}
+            style={{background: cell.figure && cell.available ? 'green' : ''}}
+        >
+            {!cell.figure && cell.available && <div className={'available'}/>}
+            {cell.figure?.logo && <img src={cell.figure.logo}></img>}
+        </div>
+    );
+};
+
+export default CellComponent;
